607e34478d447ca73df14bb5eae281ebe3a40ff4
tanomitsu2002 <64388900+tanomitsu@users.noreply.github.com>
2025-04-26 16:06:28 +0900

    refac: repositoryはuuidを使う

--- backend-go/internal/domain/repository/user.go
+++ backend-go/internal/domain/repository/user.go
@@ -2,14 +2,15 @@ package repository
 
 import (
 	"github.com/aki-13627/animalia/backend-go/ent"
+	"github.com/google/uuid"
 )
 
 type UserRepository interface {
 	Create(name, email string) (*ent.User, error)
 	ExistsEmail(email string) (bool, error)
 	FindByEmail(email string) (*ent.User, error)
-	GetById(id string) (*ent.User, error)
-	Update(id string, name string, description string, newImageKey string) error
+	GetById(id uuid.UUID) (*ent.User, error)
+	Update(id uuid.UUID, name string, description string, iconImageKey string, streakCount int) error
 	Follow(toId string, fromId string) error
 	Unfollow(toId string, fromId string) error
 }
