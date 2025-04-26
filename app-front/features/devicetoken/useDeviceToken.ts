e89cb9842f6f94a43848e522ec7de97f40aae84d
aki-13627 <aki.kaku0627@gmail.com>
2025-04-26 21:35:46 +0900

    fix: format

--- app-front/features/devicetoken/useDeviceToken.ts
+++ app-front/features/devicetoken/useDeviceToken.ts
@@ -8,30 +8,35 @@ import { z } from 'zod';
 export default function useDeviceToken() {
   const { user, token } = useAuth();
 
-  const upsertDeviceToken = useCallback(async (deviceId: string) => {
-    if (!user) return;
+  const upsertDeviceToken = useCallback(
+    async (deviceId: string) => {
+      if (!user) return;
 
-    try {
-      const notificationToken = await SecureStore.getItemAsync('notificationToken');
-      if (!notificationToken) {
-        Alert.alert('エラー', 'デバイストークンが取得できませんでした');
-        return;
+      try {
+        const notificationToken = await SecureStore.getItemAsync(
+          'notificationToken'
+        );
+        if (!notificationToken) {
+          Alert.alert('エラー', 'デバイストークンが取得できませんでした');
+          return;
+        }
+
+        const platform = Platform.OS;
+
+        await fetchApi({
+          method: 'POST',
+          path: `/devicetokens/upsert?userId=${user.id}&deviceId=${deviceId}&token=${notificationToken}&platform=${platform}`,
+          schema: z.any(),
+          options: {},
+          token,
+        });
+      } catch (error) {
+        console.error(error);
+        console.error('デバイストークンのアップサートに失敗しました', error);
       }
-
-      const platform = Platform.OS;
-
-      await fetchApi({
-        method: 'POST',
-        path: `/devicetokens/upsert?userId=${user.id}&deviceId=${deviceId}&token=${notificationToken}&platform=${platform}`,
-        schema: z.any(),
-        options: {},
-        token,
-      });
-    } catch (error) {
-      console.error(error);
-      console.error('デバイストークンのアップサートに失敗しました', error);
-    }
-  }, [user, token]);
+    },
+    [user, token]
+  );
 
   return { upsertDeviceToken };
 }
