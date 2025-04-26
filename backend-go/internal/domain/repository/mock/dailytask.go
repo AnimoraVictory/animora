adf7c034b540045b960933a066b4a94a84f728a1
tanomitsu2002 <64388900+tanomitsu@users.noreply.github.com>
2025-04-26 16:50:39 +0900

    feat: テストを追加

---
+++ backend-go/internal/domain/repository/mock/dailytask.go
@@ -0,0 +1,23 @@
+package mock
+
+import (
+	"github.com/aki-13627/animalia/backend-go/internal/domain/models"
+	"github.com/aki-13627/animalia/backend-go/internal/domain/repository"
+	"github.com/google/uuid"
+)
+
+type MockDailyTaskRepository struct {
+	CreateFunc               func(userId uuid.UUID) error
+	GetPreviousDailyTaskFunc func(userId uuid.UUID) (*models.DailyTaskWithEdges, error)
+}
+
+// Ensure MockDailyTaskRepository implements DailyTaskRepository interface
+var _ repository.DailyTaskRepository = (*MockDailyTaskRepository)(nil)
+
+func (m *MockDailyTaskRepository) Create(userId uuid.UUID) error {
+	return m.CreateFunc(userId)
+}
+
+func (m *MockDailyTaskRepository) GetPreviousDailyTask(userId uuid.UUID) (*models.DailyTaskWithEdges, error) {
+	return m.GetPreviousDailyTaskFunc(userId)
+}
