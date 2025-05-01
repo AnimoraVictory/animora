705c87d64753ae88d1fc1f85fa8fb8e759402a0b
tanomitsu2002 <64388900+tanomitsu@users.noreply.github.com>
2025-05-02 01:17:09 +0900

    feat: algorithmのURLを変更

--- cdk/lib/stack.ts
+++ cdk/lib/stack.ts
@@ -30,6 +30,75 @@ export class AnimoraStack extends cdk.Stack {
       "AWS_S3_BUCKET_NAME",
     ]);
 
+    // ECRリポジトリを参照（別のスタックから）
+    let algorithmRepo: ecr.IRepository;
+    if (props?.ecrRepositoryName) {
+      // 明示的に指定された場合はその名前で参照
+      algorithmRepo = ecr.Repository.fromRepositoryName(
+        this,
+        "ImportedAlgorithmRepo",
+        props.ecrRepositoryName
+      );
+    } else {
+      // スタックのエクスポート値から参照
+      const repoName = cdk.Fn.importValue(`AlgorithmRepoName-${env.NAME}`);
+      algorithmRepo = ecr.Repository.fromRepositoryName(
+        this,
+        "ImportedAlgorithmRepo",
+        repoName.toString()
+      );
+    }
+
+    const algorithmFnRole = new Role(this, "AlgorithmFunctionRole", {
+      assumedBy: new ServicePrincipal("lambda.amazonaws.com"),
+      managedPolicies: [
+        ManagedPolicy.fromAwsManagedPolicyName(
+          "service-role/AWSLambdaBasicExecutionRole"
+        ),
+      ],
+    });
+
+    algorithmFnRole.addToPolicy(
+      new cdk.aws_iam.PolicyStatement({
+        actions: ["s3:GetObject", "s3:PutObject", "s3:ListBucket"],
+        resources: [
+          `arn:aws:s3:::${env.AWS_S3_BUCKET_NAME}`,
+          `arn:aws:s3:::${env.AWS_S3_BUCKET_NAME}/*`,
+        ],
+      })
+    );
+
+    const algorithmLambda = new lambda.DockerImageFunction(
+      this,
+      "AlgorithmFunction",
+      {
+        code: lambda.DockerImageCode.fromEcr(algorithmRepo, {
+          tagOrDigest: "latest",
+        }),
+        role: algorithmFnRole,
+        timeout: cdk.Duration.seconds(30),
+        memorySize: 512,
+        environment: {
+          ...env,
+          TARGET_APP: "recommend_system.main:app",
+        },
+      }
+    );
+
+    const algorithmApi = new apigw.LambdaRestApi(this, "AlgorithmAPI", {
+      handler: algorithmLambda,
+      defaultCorsPreflightOptions: {
+        allowOrigins: apigw.Cors.ALL_ORIGINS,
+        allowMethods: apigw.Cors.ALL_METHODS,
+        allowHeaders: [
+          "Content-Type",
+          "X-Amz-Date",
+          "Authorization",
+          "X-Api-Key",
+        ],
+      },
+    });
+
     const apiFnRole = new Role(this, "ApiFunctionRole", {
       assumedBy: new ServicePrincipal("lambda.amazonaws.com"),
       managedPolicies: [
@@ -62,6 +131,7 @@ export class AnimoraStack extends cdk.Stack {
       ),
       environment: {
         ...env,
+        ALGORITHM_API_URL: algorithmApi.url,
       },
       role: apiFnRole,
     });
@@ -148,75 +218,6 @@ export class AnimoraStack extends cdk.Stack {
     new events.Rule(this, "DailyTaskRule", {
       schedule: events.Schedule.cron({ minute: "0", hour: "15", day: "*" }),
       targets: [new targets.LambdaFunction(dailyTaskFn)],
-    });
-
-    // ECRリポジトリを参照（別のスタックから）
-    let algorithmRepo: ecr.IRepository;
-    if (props?.ecrRepositoryName) {
-      // 明示的に指定された場合はその名前で参照
-      algorithmRepo = ecr.Repository.fromRepositoryName(
-        this,
-        "ImportedAlgorithmRepo",
-        props.ecrRepositoryName
-      );
-    } else {
-      // スタックのエクスポート値から参照
-      const repoName = cdk.Fn.importValue(`AlgorithmRepoName-${env.NAME}`);
-      algorithmRepo = ecr.Repository.fromRepositoryName(
-        this,
-        "ImportedAlgorithmRepo",
-        repoName.toString()
-      );
-    }
-
-    const algorithmFnRole = new Role(this, "AlgorithmFunctionRole", {
-      assumedBy: new ServicePrincipal("lambda.amazonaws.com"),
-      managedPolicies: [
-        ManagedPolicy.fromAwsManagedPolicyName(
-          "service-role/AWSLambdaBasicExecutionRole"
-        ),
-      ],
-    });
-
-    algorithmFnRole.addToPolicy(
-      new cdk.aws_iam.PolicyStatement({
-        actions: ["s3:GetObject", "s3:PutObject", "s3:ListBucket"],
-        resources: [
-          `arn:aws:s3:::${env.AWS_S3_BUCKET_NAME}`,
-          `arn:aws:s3:::${env.AWS_S3_BUCKET_NAME}/*`,
-        ],
-      })
-    );
-
-    const algorithmLambda = new lambda.DockerImageFunction(
-      this,
-      "AlgorithmFunction",
-      {
-        code: lambda.DockerImageCode.fromEcr(algorithmRepo, {
-          tagOrDigest: "latest",
-        }),
-        role: algorithmFnRole,
-        timeout: cdk.Duration.seconds(30),
-        memorySize: 512,
-        environment: {
-          ...env,
-          TARGET_APP: "recommend_system.main:app",
-        },
-      }
-    );
-
-    const algorithmApi = new apigw.LambdaRestApi(this, "AlgorithmAPI", {
-      handler: algorithmLambda,
-      defaultCorsPreflightOptions: {
-        allowOrigins: apigw.Cors.ALL_ORIGINS,
-        allowMethods: apigw.Cors.ALL_METHODS,
-        allowHeaders: [
-          "Content-Type",
-          "X-Amz-Date",
-          "Authorization",
-          "X-Api-Key",
-        ],
-      },
     });
   }
 }
